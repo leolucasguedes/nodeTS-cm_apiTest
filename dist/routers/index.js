d6c1376271b7047a2f5991ade593e44f42bd137c
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2023-03-19 18:49:35 -0300

    fix: /dist deleted

--- dist/routers/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import validSchema from "../middlewares/schemaValidator.js";
-import userSchema from "../schemas/userSchema.js";
-import * as service from "../controllers/index.js";
-var router = Router();
-router.post("/", validSchema(userSchema, "/"), service.postUser);
-router.get('/users', service.getUsers);
-export default router;
--- dist/schemas/userSchema.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-var userSchema = Joi.object({
-    name: Joi.string().required(),
-    age: Joi.number().min(10).max(100).required()
-});
-export default userSchema;
